055c55bf30e3a1a5ca30bac599ea08313e839984
Valenplayer3000 <64864880+Valenplayer3000@users.noreply.github.com>
2020-10-20 14:46:56 -0400

    V9.1 FB Changelog: Added new two tpye of Enemy: Heavy and Scout.

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -16,6 +16,8 @@ namespace myTiles {
     export const tile6 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
 
 }
 // Auto-generated code. Do not edit.
